5685a5e782adc6e22ec509aa947702b0ea95f6f3
Eirik Jensen <eiriktj@pm.me>
2019-10-31 18:00:20 +0100

    Add count manifestation connected to work

--- src/main/java/gs/Count.java
+++ src/main/java/gs/Count.java
@@ -1,13 +1,8 @@
 package gs;
 
-import java.util.List;
-
-import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.logging.Log;
-import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
@@ -15,28 +10,43 @@ import org.neo4j.procedure.UserFunction;
 public class Count
 {
 
-    @Context
-    public GraphDatabaseService db;
-
-    @Context
-    public Log log;
-
     @UserFunction(value = "gs.ecount")
     @Description("Count number of expressions connected to work.")
-    public String ecount(
+    public long ecount(
             @Name("node") Node node)
     {
-        Iterable<Relationship> rels = node.getRelationships(RelTypes.REALIZES);
+        Iterable<Relationship> wrels = node.getRelationships(RelTypes.REALIZES);
 
-        int counter = 0;
-        for (Object i : rels) {
+        long counter = 0;
+        for (Object i : wrels) 
+        {
             counter++;
         }
-        return Integer.toString(counter);
+        return counter;
+    }
+
+    @UserFunction(value = "gs.mcount")
+    @Description("Count number of manifestations connected to work.")
+    public long mcount(
+            @Name("node") Node node)
+    {
+        Iterable<Relationship> wrels = node.getRelationships(RelTypes.REALIZES);
+
+        long counter = 0;
+        for (Relationship rel : wrels)
+        {
+            Iterable<Relationship> erels = rel.getOtherNode(node).getRelationships(RelTypes.EMBODIES);
+            for (Object i : erels) 
+            {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     private enum RelTypes implements RelationshipType
     {
-        REALIZES
+        REALIZES,
+        EMBODIES
     }
 }
