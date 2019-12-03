5385ad5971aca730ab6fc4c7a47767e253ad97de
Eirik Jensen <eiriktj@pm.me>
2019-12-03 07:08:24 +0100

    json request get expected response

--- queries/request.py
+++ queries/request.py
@@ -2,12 +2,7 @@ import requests
 import json
 
 with open('queries.json', 'r') as f:
-    #data = f.read()
     data = json.load(f)
-url = 'url:7474/db/data/cypher/'
-headers = {'Content-type': 'application/json ', 'Accept': 'application/json;charset=UTF-8 '}
-r = requests.post(url, auth=('user', 'password'), headers=headers, data=data)
+url = 'url:7474/db/data/transaction/commit'
+r = requests.post(url, auth=('user', 'password'), json=data)
 print(r.text)
-print(r.status_code)
-print(data)
-print(type(data))
