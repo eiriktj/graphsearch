3b01e680d3dd513d7d62f9bd83e205b381ab55d8
Eirik Jensen <eiriktj@pm.me>
2019-12-06 03:19:59 +0100

    replace json queries file with search strings

--- queries/request.py
+++ queries/request.py
@@ -1,8 +1,22 @@
 import requests
-import json
 
-with open('queries.json', 'r') as f:
-    data = json.load(f)
+with open('search_strings.txt', 'r') as f:
+    search_strings = f.read().splitlines()
+
+query_string = """\
+CALL db.index.fulltext.queryNodes('work', '{search_string}') \
+YIELD node as work, score \
+RETURN work.label, score, work.pagerank, sqrt(work.pagerank * score) as calc \
+ORDER BY calc descending\
+"""
+
+data = {}
+data['statements'] = []
+for string in search_strings:
+    formatted_string = query_string.format(search_string=string)
+    data['statements'].append({'statement': formatted_string})
+
 url = 'url:7474/db/data/transaction/commit'
 r = requests.post(url, auth=('user', 'password'), json=data)
 print(r.text)
+
