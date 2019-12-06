605d0f3fb3e6db774f9ea72d5dbaa5b6f944bea2
Eirik Jensen <eiriktj@pm.me>
2019-12-06 05:18:40 +0100

    write json response to tex

--- queries/request.py
+++ queries/request.py
@@ -1,3 +1,5 @@
+from pylatex import Document, Section, Subsection, Tabular
+
 import requests
 
 with open('search_strings.txt', 'r') as f:
@@ -17,6 +19,26 @@ for string in search_strings:
     data['statements'].append({'statement': formatted_string})
 
 url = 'url:7474/db/data/transaction/commit'
-r = requests.post(url, auth=('user', 'password'), json=data)
-print(r.text)
+r = requests.post(url, auth=('username', 'password'), json=data)
 
+with open('results.json', 'w') as f:
+    f.write(r.text)
+
+results = r.json()['results']
+
+columns = results[0]['columns']
+
+doc = Document()
+
+with doc.create(Section('Tables')):
+    for i in range(len(search_strings)):
+        with doc.create(Subsection(search_strings[i])):
+            with doc.create(Tabular('|'+'l|'*len(columns))) as table:
+                table.add_hline()
+                table.add_row((columns[0],columns[1],columns[2],columns[3]))
+                rows = results[i]['data']
+                for row in rows:
+                    table.add_hline()
+                    table.add_row((row['row'][0],row['row'][1],row['row'][2],row['row'][3]))
+                table.add_hline()
+doc.generate_pdf('tables')
