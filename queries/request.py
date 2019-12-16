d6088302b2d717d1af8f6b1960e5d8ba4da5f539
Eirik Jensen <eiriktj@pm.me>
2019-12-16 16:39:00 +0100

    Limit number of results. Round score strings. Formatting changes to latex table.

--- queries/request.py
+++ queries/request.py
@@ -1,4 +1,5 @@
 from pylatex import Document, Section, Subsection, LongTable
+from pylatex.utils import bold
 
 import requests
 
@@ -6,10 +7,12 @@ with open('search_strings.txt', 'r') as f:
     search_strings = f.read().splitlines()
 
 query_string = """\
-CALL db.index.fulltext.queryNodes('work', '{search_string}') \
-YIELD node as work, score \
-RETURN work.label, score, work.pagerank, sqrt(work.pagerank * score) as calc \
-ORDER BY calc descending\
+CALL        db.index.fulltext.queryNodes('work', '{search_string}') \
+YIELD       node as work, score \
+RETURN      work.label, score, work.pagerank AS pagerank, \
+            sqrt(work.pagerank * score) AS calc \
+ORDER BY    calc descending \
+LIMIT       10 \
 """
 
 data = {}
@@ -18,8 +21,8 @@ for string in search_strings:
     formatted_string = query_string.format(search_string=string)
     data['statements'].append({'statement': formatted_string})
 
-url = 'url:7474/db/data/transaction/commit'
-r = requests.post(url, auth=('username', 'password'), json=data)
+uri = 'url:7474/db/data/transaction/commit'
+r = requests.post(uri, auth=('username', 'password'), json=data)
 
 with open('results.json', 'w') as f:
     f.write(r.text)
@@ -34,12 +37,14 @@ doc = Document()
 with doc.create(Section('Tables')):
     for i in range(len(search_strings)):
         with doc.create(Subsection(search_strings[i])):
-            with doc.create(LongTable('|'+'p{5cm}|'+'p{3cm}|'*(len(columns)-1))) as table:
+            with doc.create(LongTable('|'+'p{8cm}|'+'p{1cm}|'*(len(columns)-1))) as table:
                 table.add_hline()
-                table.add_row((*columns))
+                table.add_row((*columns),mapper=bold)
                 rows = results[i]['data']
                 for row in rows:
                     table.add_hline()
-                    table.add_row((*row['row']))
+                    label = row['row'].pop(0)
+                    scores = [ round(float(elem), 4) for elem in row['row'] ]
+                    table.add_row((label, *scores))
                 table.add_hline()
 doc.generate_pdf('tables', compiler='lualatex')
